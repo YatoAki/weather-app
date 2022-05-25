3c7760ecf580e434d3e6de6bda8ac7784218a9d1
Yato Aki <yatoakii@gmail.com>
2022-05-25 18:26:19 +0630

    Added Backgroundimage functions

--- src/script/index.js
+++ src/script/index.js
@@ -1,1 +1,13 @@
 import '../style/style.css';
+import night from "../assets/night.jpg";
+import day from "../assets/day.jpg";
+
+function changeDayBackground() {
+  document.body.style.backgroundImage = `url(${day})`;
+}
+
+function changeNightBackground() {
+  document.body.style.backgroundImage = `url(${night})`;
+}
+
+changeDayBackground();
