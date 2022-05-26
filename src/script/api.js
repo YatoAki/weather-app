21bd8fc8063896a3503b1aefa6f8249dde333c2b
Yato Aki <yatoakii@gmail.com>
2022-05-26 20:45:40 +0630

    Improve code quailty

--- src/script/api.js
+++ src/script/api.js
@@ -1,17 +1,18 @@
 import UI from "./ui.js"
 
 export default class API{
-  static start(cityName){
-    const url = API.makeRequestUrl(cityName);
-    const initialData = API.requestData(url);
+  static async start(cityName){
+    const initialData = await API.requestInitialData(cityName);
     console.log(initialData);
+    // const detailData = await API.requestDetailData(url);
   }
 
-  static makeRequestUrl(cityName){
+  static makeInitialRequestUrl(cityName){
     return `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&APPID=1bd3a1f212c7d6849195a18bd92cac18`;
   }
 
-  static async requestData(url){
+  static async requestInitialData(cityName){
+    const url = API.makeInitialRequestUrl(cityName);
     try {
       const response = await fetch(url,{mode:'cors'});
       const weatherData = await response.json();
--- src/script/ui.js
+++ src/script/ui.js
@@ -35,4 +35,8 @@ export default class UI{
     input.value = "";
     error.textContent = "";
   }
+
+  static updateUI(){
+    
+  }
 }
